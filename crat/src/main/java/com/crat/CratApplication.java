20cfbb3ca4181c33b254c6c03a1e9019b13a04fa
shubham <shubham@shubham-PC>
2021-07-16 14:52:41 +0530

    commit

---
+++ crat/src/main/java/com/crat/CratApplication.java
@@ -0,0 +1,13 @@
+package com.crat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CratApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CratApplication.class, args);
+	}
+
+}
---
+++ crat/src/test/java/com/crat/CratApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CratApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
